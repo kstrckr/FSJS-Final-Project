f6b4a9b20acd1b998729e26a472ba65904772e30
Kurt <ksphotopro@gmail.com>
2017-06-18 21:32:23 -0400

    so far so good ScoreBoard and GameBoard working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GameBoard } from './game-board.component';
+import { ScoreBoard } from './score-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameBoard,
+    ScoreBoard
   ],
   imports: [
     BrowserModule
