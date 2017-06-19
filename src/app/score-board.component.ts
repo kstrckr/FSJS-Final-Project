bfb576b5a932b8f4ee2ef4572d32b0a9a8e31883
Kurt <ksphotopro@gmail.com>
2017-06-18 21:32:45 -0400

    Custom Font added

---
+++ src/app/score-board.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'score-board',
+    templateUrl: './score-board.component.html',
+    styleUrls: ['./score-board.component.css']
+})
+
+export class ScoreBoard {   }
